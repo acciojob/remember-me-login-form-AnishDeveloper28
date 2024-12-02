fb5c6e94ae2ccbd9d18ef47bba1589769125cca7
Anish Jain <anishsinghvi4@gmail.com>
2024-12-02 21:50:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+const loginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const rememberMeCheckbox = document.getElementById("checkbox");
+const existingUserBtn = document.getElementById("existing");
+
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if (savedUsername && savedPassword) {
+    existingUserBtn.style.display = "block";
+}
+
+oginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (rememberMeCheckbox.checked) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+    } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+    }
+    alert(`Logged in as ${username}`);
+		if (rememberMeCheckbox.checked) {
+        existingUserBtn.style.display = "block";
+    }
+});
+
+existingUserBtn.addEventListener("click", () => {
+    alert(`Logged in as ${savedUsername}`);
+});
